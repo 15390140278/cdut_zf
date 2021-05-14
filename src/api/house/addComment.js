6ea5e100bca575d946339c7ea1c2b9ab7269596a
Znf <8365952+buttery_fly@user.noreply.gitee.com>
2021-05-14 20:06:55 +0800

    修改

--- src/api/house/addComment.js
+++ src/api/house/addComment.js
@@ -1,7 +1,11 @@
 import request from '../request'
-
+import store from '../../store/index'
 const addCommentRequest = data => {
-  return request.post('/comment/addComment', request.qs(data))
+  return request.post('/comment/addComment', request.qs(data), {
+    headers: {
+      token: store.state.user.token
+    }
+  })
 }
 
 export default addCommentRequest
